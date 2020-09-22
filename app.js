94fe88045199e572f9caf4e657de9197e1c4d35c
Kuwamura3 <yasu.metallurgy.th@gmail.com>
2020-09-22 18:06:34 +0900

    [Finish] J-2

--- app.js
+++ app.js
@@ -1,2 +1,37 @@
-var hello = "Hello World";
-alert(hello);
+var num1 = 5;
+var num2 = 10;
+var wa;
+var sa;
+var seki;
+var shou;
+
+wa = num1 + num2;
+sa = num1 - num2;
+seki = num1 * num2;
+shou = num1 / num2;
+
+alert(
+  num1 +
+    "+" +
+    num2 +
+    "=" +
+    wa +
+    "　　　" +
+    num1 +
+    "-" +
+    num2 +
+    "=" +
+    sa +
+    "　　　" +
+    num1 +
+    "*" +
+    num2 +
+    "=" +
+    seki +
+    "　　　" +
+    num1 +
+    "/" +
+    num2 +
+    "=" +
+    shou
+);
